4a239075356ea55cd95b5cd0a87924be1ab2d9b9
Guy Halfon <guyh@wix.com>
2019-04-30 00:03:09 +0300

    first ver

--- run.py
+++ run.py
@@ -1,25 +1,24 @@
 # nox90.com
 # python 2.7 so it can run on Qradar servers...
 # Close an offense via API
-# usage run.py <hostname> <offense_id> <close reason id>
-
-APIKEY = ''
-
+# usage run.py <apiToken> <hostname> <offense_id> <close reason id>
 
 import requests
 import sys
 
+apiToken = sys.argv[0]
+
 # Get host
-endpoint = 'https://'+sys.argv[0]+'/api/siem/offenses/'
+endpoint = 'https://'+sys.argv[1]+'/api/siem/offenses/'
 
 # Get offense id
-offenseId = sys.argv[1]
+offenseId = sys.argv[2]
 
 # Get close reason id
-closeReasonId = sys.argv[2]
+closeReasonId = sys.argv[3]
 
 # strings assemble
 url = endpoint+offenseId+'?closing_reason_id='+closeReasonId+'&status=CLOSED'
 
-r = requests.post(url)
-print(r.status_code, r.reason)
+r = requests.post(url, headers={'Accept': 'application/json', 'SEC': apiToken})
+print(r.status_code, r.reason, r.text)
